8ae83b9f62bfd4f550f1cc702a0f15b4695203eb
Wasin pansophon <wasin@orcsoft.co.th>
2019-10-24 14:07:09 +0700

    implement method generateCaptcha

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module ci.inno.ktb/wpansophon/captcha
 
 go 1.12
 
-require github.com/stretchr/testify v1.4.0
+require (
+	github.com/divan/num2words v0.0.0-20170904212200-57dba452f942
+	github.com/stretchr/testify v1.4.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
 
+	"github.com/divan/num2words"
+)
+
+func generateCaptcha(f, lo, op, ro int) string {
+	lor := strconv.Itoa(lo)
+	ror := strconv.Itoa(ro)
+
+	switch f {
+	case 0:
+		lor = num2words.Convert(lo)
+	case 1:
+		ror = num2words.Convert(ro)
+	}
+
+	opr := "+"
+	switch op {
+	case 0:
+		opr = "+"
+	case 1:
+		opr = "-"
+	case 2:
+		opr = "*"
+	}
+
+	return fmt.Sprintf("%s %s %s", lor, opr, ror)
 }
